0f6d7a8ba376cadfaae7b4c3804da5204be42c1f
Opilane TTHK <74781679+korneff1337@users.noreply.github.com>
2026-01-21 10:02:49 +0200

    Initial commit

---
+++ Meetodid/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Meetodid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> filmid = GetMovies();
+            Console.WriteLine("Milline on sinu lemmikfilm?");
+            string lemmikfilm = ReadAnswer();
+            DoesMovieExist(filmid, lemmikfilm);
+            filmid = DoYouLikeThisMovie(filmid, "Predator: Badlands 2025");
+            //foreach (string item in filmid)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+
+        public static List<string> DoYouLikeThisMovie(List <string> collection, string movieToAdd)
+        {
+            if (collection.Contains(movieToAdd) == false)
+            {
+                Console.WriteLine($"Kas sulle meeldib {movieToAdd}?");
+                string vastus = ReadAnswer();
+
+                if (vastus == "Jah")
+                {
+                    collection.Add(movieToAdd);
+                    Console.WriteLine("Lisasin filmi sulle");
+                }
+                else
+                {
+                    Console.WriteLine("Aga miks? See on ju hea film");
+                }
+            }
+            return collection;
+        }
+
+
+        public static void DoesMovieExist(List<string> collection, string findThis)
+        {
+            bool itExists = false;
+            foreach (var item in collection)
+            {
+                if (item == findThis)
+                {
+                    itExists = true;
+                }
+            }
+            if (itExists == true )
+            {
+                Console.WriteLine("Näe mäletasid ikka!");
+            }
+            else
+            {
+                Console.WriteLine("A kus sinu lemmikfilm?");
+            }
+
+        }
+
+        public static string ReadAnswer()
+        {
+            string sisend = "";
+            while (sisend == "")
+            {
+                sisend = Console.ReadLine();
+            }
+            return sisend;
+        }
+
+
+        public static List<string> GetMovies()
+        {
+            Console.WriteLine("Mis on siu lemmikfilmid, sisesta ükshaaval, kui sisestada rohkem ei taha\nsisesta \"ei ole\" ");
+            string sisestus = "";
+            List <string> siinOnFilmid = new List<string>();
+            while (sisestus != "ei ole")
+            {
+                Console.WriteLine("Järgmine film:");
+                sisestus = Console.ReadLine();
+                if (sisestus != "ei ole")
+                {
+                    siinOnFilmid.Add(sisestus);
+                }
+            }
+            return siinOnFilmid;
+        }
+        
+    }
+}
