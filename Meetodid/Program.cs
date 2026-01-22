14dc9da5ddcb179ec632fc3816f4bce01959441c
RobinHarm <74781679+korneff1337@users.noreply.github.com>
2026-01-22 09:36:35 +0200

    filmid

--- Meetodid/Program.cs
+++ Meetodid/Program.cs
@@ -9,13 +9,40 @@
             string lemmikfilm = ReadAnswer();
             DoesMovieExist(filmid, lemmikfilm);
             filmid = DoYouLikeThisMovie(filmid, "Predator: Badlands 2025");
-            //foreach (string item in filmid)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            List<string> otsitavadFilmid = new List<string> { "Terminator", "Vanamehe Film", "Kratt" };
+            IdentifyMovies(filmid, otsitavadFilmid);
         }
 
-        public static List<string> DoYouLikeThisMovie(List <string> collection, string movieToAdd)
+        public static void IdentifyMovies(List<string> collection, List<string> filter)
+        {
+            //* Terminaatori kohta "Ill be back"
+            //* Vanamehe filmi kohta "õnikurs"
+            //* Krati kohta "Vaata et ta sul tehisplära ajama ei hakka"
+            string messages = "";
+            foreach (var movie in collection)
+            {
+                int itemnr = 0;
+                foreach (var filterItem in filter)
+                {
+                    if (itemnr == 0)
+                    {
+                        messages += "I'll be back.\n";
+                    }
+                    else if (itemnr == 1)
+                    {
+                        messages += "$nikurs\n";
+                    }
+                    else if (itemnr == 2)
+                    {
+                        messages += "Vaata et ta sul tehisplära ajama ei hakka\n";
+                    }
+                    itemnr++;
+                }
+                itemnr = 0;
+            }
+            Console.WriteLine(messages);
+        }
+        public static List<string> DoYouLikeThisMovie(List<string> collection, string movieToAdd)
         {
             if (collection.Contains(movieToAdd) == false)
             {
@@ -46,7 +73,7 @@
                     itExists = true;
                 }
             }
-            if (itExists == true )
+            if (itExists == true)
             {
                 Console.WriteLine("Näe mäletasid ikka!");
             }
@@ -72,7 +99,7 @@
         {
             Console.WriteLine("Mis on siu lemmikfilmid, sisesta ükshaaval, kui sisestada rohkem ei taha\nsisesta \"ei ole\" ");
             string sisestus = "";
-            List <string> siinOnFilmid = new List<string>();
+            List<string> siinOnFilmid = new List<string>();
             while (sisestus != "ei ole")
             {
                 Console.WriteLine("Järgmine film:");
@@ -84,6 +111,6 @@
             }
             return siinOnFilmid;
         }
-        
+
     }
 }
